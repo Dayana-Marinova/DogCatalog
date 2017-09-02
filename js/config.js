4fd2aff16bbd0c2030ce611eec2333f816263e3b
lepovica <lepo333@abv.bg>
2017-09-03 00:21:46 +0300

    didi e zle

--- js/config.js
+++ js/config.js
@@ -1,5 +1,8 @@
 var dog_api = 'https://dog.ceo/api/'
+var breed = 'breed/'
 var breeds = 'breeds/'
 var random_image = 'image/random'
-var list = 'list'
+var random_images = 'images/randon'
+var list = 'list/'
 var images = 'images'
+
--- js/dogs.js
+++ js/dogs.js
@@ -1,8 +1,24 @@
 var Dogs = (function ($) {
+    sep = "/";
     return {
-        randomDog: function ( ) {
+        randomDog: function () {
             return $.ajax({
                 url: dog_api + breeds + random_image
+            });
+        },
+        getBreeds: function() {
+            return $.ajax({
+                url: dog_api + breeds + list
+            });
+        },
+        getBreed: function(breedName) {
+            return $.ajax({
+                url: dog_api + breed + breedName + sep + images
+            });
+        },
+        getSubBreeds: function(breedName, subBreedName) {
+            return $.ajax({
+                url: dog_api + breed + breedName + sep + subBreedName + sep + images
             });
         }
     };
